73f23f74c73517f9461312f8f667da38af888dba
Jeff Hykin <jeff.hykin@gmail.com>
2024-03-08 09:34:01 -0600

    add the physical test

--- lab1/support/survivor_buddy_serial/serial_com.py
+++ lab1/support/survivor_buddy_serial/serial_com.py
@@ -189,3 +189,38 @@ class SurvivorBuddySerial:
         
         # add all of them at the end to reduce thread-locking overhead
         self.scheduled_actions += scheduled_actions
+
+if __name__ == '__main__':
+    print("testing survivor_buddy connection")
+    survivor_bud = SurvivorBuddySerial(
+        port_address=None,
+        baud_rate=115200,
+        windows_default_address="COM4",
+        linux_default_address="/dev/ttyUSB0",
+        mac_default_address=None, # mac auto-detects name
+        inital_positions=[90,90,90,90],
+        logging=False,
+        include_legacy_survivor_buddy_support=True,
+    )
+    # 
+    # cli loop 
+    # 
+    print(f'''Type q [Enter] to quit''')
+    while 1:
+        response = input("\nEnter joint positions. Units=degrees. (space or comma separated)\n")
+        if response.lower() == 'q' or response.lower() == 'quit' or response.lower() == 'exit':
+            break
+        
+        chunks = [""]
+        for each in response:
+            is_valid = each == "." or each == "-" or each.isdigit()
+            if not is_valid:
+                chunks.append("")
+            if is_valid:
+                chunks[-1]+=each
+        
+        if len(chunks[-1]) == 0:
+            chunks.pop()
+        
+        new_joints = [ int(each) for each in chunks ]
+        survivor_bud.set_joints(*new_joints, speed=40)
